35257ae15943a9f6d67fdde61f6b019487fca277
Duke239 <duke.mcintyre.dm@gmail.com>
2025-06-30 16:24:57 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,10 @@
-# db.py
 from motor.motor_asyncio import AsyncIOMotorClient
-import os
 
-client: AsyncIOMotorClient = None
-db = None
-
-async def connect_to_mongo():
-    global client, db
+async def connect_to_mongo(app):
     client = AsyncIOMotorClient(os.environ["MONGO_URL"])
     db = client[os.environ.get("DB_NAME", "lda_timetracking")]
+    app.state.mongo_client = client
+    app.state.db = db
 
-async def close_mongo_connection():
-    client.close()
+async def close_mongo_connection(app):
+    app.state.mongo_client.close()
